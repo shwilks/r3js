c429d7b49c4cb389283f8b9cc6fb8241d349079b
Sam Wilks <sw463@cam.ac.uk>
2021-03-11 19:42:06 +0000

    make object naming consistent with S3 print method

--- R/3JSmethods.R
+++ R/3JSmethods.R
@@ -3,13 +3,14 @@
 #'
 #' The print method for the data3js object, by default plotting the htmlwidget.
 #'
-#' @param data3js The r3js data object
+#' @param x The data3js object
+#' @param ... Additional arguments, not used
 #'
 #' @export
 #'
-print.data3js <- function(data3js){
+print.data3js <- function(x, ...){
 
-  print(r3js(data3js))
+  print(r3js(x))
 
 }
 
