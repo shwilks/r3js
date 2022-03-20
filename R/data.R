ce59fbd7b800273a60bfebd8888d79ae95e5b770
Sam Wilks <sw463@cam.ac.uk>
2022-03-20 23:20:28 +0000

    Add teapot data documentation

---
+++ R/data.R
@@ -0,0 +1,16 @@
+
+#' Utah Teapot
+#'
+#' The Utah teapot is a classic computer graphics example. This data set
+#' contains a representation in terms of triangles. This is taken from the
+#' `misc3d` package.
+#'
+#' @format A list with components vertices and edges. vertices is a 1976 by 3
+#'   numeric matrix of the coordinates of the vertices. edges is a 3751 by 3
+#'   integer matrix of the indices of the triangles.
+#'
+#' @source Taken from the `misc3d` package, in turn from the netCDF file that
+#'   was at one time made available by Dave Forrest at
+#'   \url{http://www.maplepark.com/~drf5n/extras/teapot.nc}.
+#'
+"teapot"
