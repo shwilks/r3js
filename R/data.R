210216bfa7626d466779f54a96cf3af79e47fb3b
Sam Wilks <sw463@cam.ac.uk>
2022-06-10 20:21:05 +0100

    Remove invalid url

--- R/data.R
+++ R/data.R
@@ -9,8 +9,6 @@
 #'   numeric matrix of the coordinates of the vertices. edges is a 3751 by 3
 #'   integer matrix of the indices of the triangles.
 #'
-#' @source Taken from the `misc3d` package, in turn from the netCDF file that
-#'   was at one time made available by Dave Forrest at
-#'   \url{http://www.maplepark.com/~drf5n/extras/teapot.nc}.
+#' @source Taken from the `misc3d` package
 #'
 "teapot"
