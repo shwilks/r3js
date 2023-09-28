3d0b302d00fe6a0ef0e662b7035b3bcc8ba8dc27
shwilks <sw463@cam.ac.uk>
2023-09-28 08:25:16 +0000

    Deploying to gh-pages from @ shwilks/r3js@a229d3d53037d1cf32455f0a31dd2b1798f3134b 🚀

--- articles/plot-from-scratch_files/r3js-1.0.0/functions/load.js
+++ articles/plot-from-scratch_files/r3js-1.0.0/functions/load.js
@@ -5,6 +5,23 @@ R3JS.Viewer.prototype.load = function(plotData, settings = {}){
     this.viewport.hidePlaceholder();
     this.contentLoaded = true;
     this.settings = settings;
+    this.id = settings.ID;
+    this.container.id = this.id;
+
+    // Link any other viewers already loaded
+    this.linkedviewers = [];
+    if (settings.linked !== undefined) {
+        this.linkedids = settings.linked;
+        for (var i=0; i<this.linkedids.length; i++) {
+
+            let id = this.linkedids[i];
+            let linked = document.getElementById(id);
+            if (id != this.id && linked && this.linkedviewers.indexOf(linked.viewer) === -1) {
+                this.linkedviewers.push(linked.viewer);
+            }
+
+        }
+    }
 
     // Set plot title
     if (settings.title !== undefined) this.name = settings.title;
@@ -25,6 +42,9 @@ R3JS.Viewer.prototype.load = function(plotData, settings = {}){
         dimensions : 3
     });
 
+    // Clear the plot
+    this.scene.empty();
+
     // Populate the plot
     this.scene.populatePlot(plotData);
 
@@ -35,7 +55,7 @@ R3JS.Viewer.prototype.load = function(plotData, settings = {}){
 
     // Add any toggles
     this.addToggles();
-	
+
     // Lights
     this.scene.clearLights();
 
@@ -72,6 +92,13 @@ R3JS.Viewer.prototype.load = function(plotData, settings = {}){
     }
 
     this.render();
+
+    // Add a plot loaded event
+    window.addEventListener('r3jsPlotLoaded', e => {
+        if (e.detail.id != this.id && this.linkedids.indexOf(e.detail.id) !== -1 && this.linkedviewers.indexOf(e.detail) === -1) {
+            this.linkedviewers.push(e.detail);
+        }
+    });
 
     // Dispatch a plot loaded event
     window.dispatchEvent(
